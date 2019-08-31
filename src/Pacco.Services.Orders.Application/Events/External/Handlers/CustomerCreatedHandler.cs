2eb360129379296933e72e9f68607179c3b61101
GooRiOn <foreverframenet@gmail.com>
2019-08-31 20:36:33 +0200

    Added missing event log messages

--- src/Pacco.Services.Orders.Application/Events/External/Handlers/CustomerCreatedHandler.cs
+++ src/Pacco.Services.Orders.Application/Events/External/Handlers/CustomerCreatedHandler.cs
@@ -1,6 +1,6 @@
 using System.Threading.Tasks;
 using Convey.CQRS.Events;
-using Microsoft.Extensions.Logging;
+using Pacco.Services.Orders.Application.Exceptions;
 using Pacco.Services.Orders.Core.Entities;
 using Pacco.Services.Orders.Core.Repositories;
 namespace Pacco.Services.Orders.Application.Events.External.Handlers
@@ -8,24 +8,20 @@ namespace Pacco.Services.Orders.Application.Events.External.Handlers
     public class CustomerCreatedHandler : IEventHandler<CustomerCreated>
     {
         private readonly ICustomerRepository _customerRepository;
-        private readonly ILogger<CustomerCreatedHandler> _logger;
 
-        public CustomerCreatedHandler(ICustomerRepository customerRepository, ILogger<CustomerCreatedHandler> logger)
+        public CustomerCreatedHandler(ICustomerRepository customerRepository)
         {
             _customerRepository = customerRepository;
-            _logger = logger;
         }
 
         public async Task HandleAsync(CustomerCreated @event)
         {
             if (await _customerRepository.ExistsAsync(@event.CustomerId))
             {
-                _logger.LogWarning($"Customer with id: {@event.CustomerId} was already added.");
-                return;
+                throw new CustomerAlreadyAddedException(@event.CustomerId);
             }
 
             await _customerRepository.AddAsync(new Customer(@event.CustomerId));
-            _logger.LogInformation($"Added a customer with id: {@event.CustomerId}");
         }
     }
 }
