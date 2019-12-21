d114e6c32c3b2ee23b7596624d73788f92e91943
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-12-21 13:55:33 +0100

    Travis docker tags fix

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -13,7 +13,7 @@ case "$TRAVIS_BRANCH" in
     ;;    
 esac
 
-REPOSITORY=$DOCKER_USERNAME/pacco.services.orders:$TAG
+REPOSITORY=$DOCKER_USERNAME/pacco.services.orders
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t $REPOSITORY:$TAG -t $REPOSITORY:$VERSION_TAG .
